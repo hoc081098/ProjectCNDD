a26eaf5d570ef23cd9624d668d14aa8b7728f474
phong016688 <phong016688@gmail.com>
2018-09-20 17:49:55 +0700

    Chinh sua

---
+++ app/src/main/java/com/pkhh/projectcndd/ui/post/Activity_district.java
@@ -0,0 +1,17 @@
+package com.pkhh.projectcndd.ui.post;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pkhh.projectcndd.R;
+
+public class Activity_district extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_district);
+
+    }
+}
--- app/src/main/java/com/pkhh/projectcndd/ui/post/Fragment2.java
+++ app/src/main/java/com/pkhh/projectcndd/ui/post/Fragment2.java
@@ -1,5 +1,6 @@
 package com.pkhh.projectcndd.ui.post;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,10 +11,13 @@ import com.pkhh.projectcndd.R;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
-public class Fragment2 extends Fragment {
-    private TextView textView;
+public class Fragment2 extends Fragment implements View.OnClickListener {
+    private ConstraintLayout constraintLayout_province;
+    private ConstraintLayout constraintLayout_district;
+    private ConstraintLayout constraintLayout_wards;
 
     @Nullable
     @Override
@@ -24,10 +28,39 @@ public class Fragment2 extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        textView = view.findViewById(R.id.textView2);
+        initView(view);
+        constraintLayout_province.setOnClickListener(this);
+        constraintLayout_district.setOnClickListener(this);
+        constraintLayout_wards.setOnClickListener(this);
+    }
+
+    private void initView(View view) {
+        constraintLayout_province = view.findViewById(R.id.layout_province);
+        constraintLayout_district = view.findViewById(R.id.layout_districts);
+        constraintLayout_wards = view.findViewById(R.id.layout_wards);
     }
 
     public void updateText(CharSequence s) {
-        textView.setText(s);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.layout_province:
+                startActivityForResult(new Intent(requireContext(), Activity_province.class), 0);
+                break;
+            case R.id.layout_districts:
+                startActivity(new Intent(requireContext(), Activity_district.class));
+        }
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0) {
+
+        }
     }
 }
